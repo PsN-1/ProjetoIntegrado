e016550e7aa3348cc11703a780cb849ea7c863ed
Pedro Neto <pedro.santosnt@hotmail.com>
2022-11-14 21:04:15 -0300

    fix carousel text on small windows

--- frontend/src/Store/MainPage/components/Carousel.jsx
+++ frontend/src/Store/MainPage/components/Carousel.jsx
@@ -1,27 +1,49 @@
-import { Box, Card, CardMedia, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 
-const image = "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+const image =
+  "https://images.unsplash.com/photo-1472851294608-062f824d29cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
 const Carousel = (props) => {
   return (
     <Box paddingX={5} paddingY={3}>
-      <Card elevation={3} position="relative">
-        <CardMedia height={200} component="img" src={image} />
-        <Typography
-          sx={{ textShadow: "0px 4px 4px #000000" }}
-          gutterBottom
-          variant="h1"
-          component="h1"
-          position="absolute"
-          top="16%"
-          width="100%"
-          textAlign="center"
-          color="White"
-          backgroundColor="none"
-          fontFamily="Comic Sans MS"
+      <Paper
+        elevation={3}
+        sx={{
+          position: "relative",
+          backgroundColor: "grey.800",
+          color: "#fff",
+          mb: 4,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundImage: `url(${image})`,
+        }}
+      >
+        <Box
+          sx={{
+            position: "relative",
+            p: { xs: 2, md: 2 },
+            pr: { md: 0 },
+          }}
         >
-          {props.title}
-        </Typography>
-      </Card>
+          <Typography
+            component="h1"
+            variant="h1"
+            color="inherit"
+            gutterBottom
+            sx={{ textShadow: "0px 4px 4px #000000" }}
+            position="relative"
+            height="100%"
+            width="100%"
+            justifyContent="center"
+            alignItems="center"
+            display="flex"
+            backgroundColor="none"
+            fontFamily="Comic Sans MS"
+          >
+            {props.title}
+          </Typography>
+        </Box>
+      </Paper>
     </Box>
   );
 };
